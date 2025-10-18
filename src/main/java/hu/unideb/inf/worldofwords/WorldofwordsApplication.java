c8527e8f3f7f34e11c6dd20cdba9422d7df14e54
Kasztl Richard <richardhun@mailbox.unideb.hu>
2025-10-18 19:43:02 +0300

    Initialize project

---
+++ src/main/java/hu/unideb/inf/worldofwords/WorldofwordsApplication.java
@@ -0,0 +1,13 @@
+package hu.unideb.inf.worldofwords;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorldofwordsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorldofwordsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hu/unideb/inf/worldofwords/WorldofwordsApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.unideb.inf.worldofwords;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorldofwordsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
