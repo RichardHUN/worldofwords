8eac155b5f165faeea2d3d46839fb1a9947f4ab3
Kasztl Richard <richardhun@mailbox.unideb.hu>
2025-11-03 16:27:43 +0100

    Add API endpoint to test reading cities from DB

---
+++ src/main/java/hu/unideb/inf/worldofwords/web/GameController.java
@@ -0,0 +1,42 @@
+package hu.unideb.inf.worldofwords.web;
+
+import lombok.NonNull;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public interface GameController {
+
+    /**
+     * Used to get the random letter for the game.
+     * @return a random letter
+     */
+    @GetMapping("/game/letter")
+    Character letter();
+
+    /**
+     * Used to submit the given answers for the game.
+     * @param given the words given by the player
+     */
+    @PostMapping("/game/submit")
+    void submit(@NonNull @RequestBody String given);
+
+    /**
+     * Used to get the current score of the player.
+     * @return null if the player has not submitted any answers yet, otherwise the current score
+     */
+    @GetMapping("/game/score")
+    Integer score();
+
+    //temp
+    @GetMapping("/testCountries")
+    List<String> testCountries();
+
+    @GetMapping("/testCities")
+    List<String> testCities();
+
+}
---
+++ src/main/java/hu/unideb/inf/worldofwords/web/GameControllerImpl.java
@@ -0,0 +1,57 @@
+package hu.unideb.inf.worldofwords.web;
+
+import hu.unideb.inf.worldofwords.repository.CityRepository;
+import hu.unideb.inf.worldofwords.repository.CountryRepository;
+import lombok.AllArgsConstructor;
+import lombok.NonNull;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@AllArgsConstructor
+public class GameControllerImpl implements GameController{
+
+    private final CountryRepository countryRepository;
+    private final CityRepository cityRepository;
+
+    /**
+     * Used to get the random letter for the game.
+     *
+     * @return a random letter
+     */
+    @Override
+    public Character letter() {
+        return null;
+    }
+
+    /**
+     * Used to submit the given answers for the game.
+     *
+     * @param given the words given by the player
+     */
+    @Override
+    public void submit(@NonNull String given) {
+
+    }
+
+    /**
+     * Used to get the current score of the player.
+     *
+     * @return null if the player has not submitted any answers yet, otherwise the current score
+     */
+    @Override
+    public Integer score() {
+        return 0;
+    }
+
+    @Override
+    public List<String> testCountries() {
+        return countryRepository.findAll().getFirst().getCountries();
+    }
+
+    @Override
+    public List<String> testCities() {
+        return cityRepository.findAll().getFirst().getCities();
+    }
+}
